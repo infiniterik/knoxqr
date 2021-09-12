682155a1ec05645a253bc31b0d3cfc9670fc2e32
Rik <rbose@knox.edu>
2021-09-12 15:53:49 -0500

    instructions for attendees

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <QRCodeStyling />
+  <div class="section is-small has-text-left">
+    <h1 class="title">Attendance Forms</h1>
+    <ol type="1">
+      <li>Follow the relevant link (below) to fill in the event name for your attendance form</li>
+        <li>Copy the link using the popup at the bottom of Google Sheets</li>
+        <li>Paste the copied link into the text box on this page</li>
+        <li>Download your QR code and print!</li>
+    </ol>
+    </div>
+  <div class="section is-small has-text-left">
+    <h2 class="title">Forms!</h2>
+    <ul>
+      <li><a class="tag is-large is-info" href="https://docs.google.com/forms/d/1mBYws_dxEeOmWux2hmsDrDiZ30JLD8Vnoc1KoA5sJJU/prefill">LLC 2021</a></li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
